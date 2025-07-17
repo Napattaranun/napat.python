5f339eb1097c98875a1c23418155882f7c1af15b
Napattaranun Mokran <napattaranun.m@ku.th>
2025-07-17 15:32:43 +0700

    napat

--- assignments/week02/quiz.py
+++ assignments/week02/quiz.py
@@ -20,14 +20,12 @@ Below 18.5: Underweight
 
 
 """
-Question 2: Currency Converter (20 points)
 
-Write a program that converts between Thai Baht (THB) and US Dollars (USD).
-Requirements:
+BMI Categories:
 
-Ask user to choose conversion direction (THB to USD or USD to THB)
-Ask for the amount to convert
-Use exchange rate: 1 USD = 35.5 THB
-Display result with 2 decimal places
-Show the calculation formula used
+Below 18.5: Underweight
+18.5 - 24.9: Normal weight
+25.0 - 29.9: Overweight
+30.0 and above: Obese
+
 """
